286b6ffec206ae6b372283b9a4c4f971b7de9d86
Austin Doorlag <addoorla@mtu.edu>
2021-11-03 23:20:55 -0400

    Added scoring calculation

--- Assets/goal.cs
+++ Assets/goal.cs
@@ -1,10 +1,18 @@
 using UnityEngine;
+using System;
 
 public class goal : MonoBehaviour
 {
     void OnTriggerEnter2D ()
     {
-        Debug.Log("You Won!");
+        Debug.Log("You Won! Score:  "+computeScore(Time.time));
         Time.timeScale = 0;
     }
+	
+	int computeScore (float time)
+	{
+		time = 30f - time;	
+		time = (time > 0) ? (time * 33.33f) : 0f;
+		return (int) Math.Round(time);
+	}
 }
