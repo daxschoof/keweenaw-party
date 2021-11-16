981e6ac4a5d941f64481d492b5e1c6b37c599d83
Reotang <reot@mtu.edu>
2021-11-15 22:36:32 -0500

    Implemented Game Logic
    Piano Tiles is able to be clicked on or using asdf. The game will end if one tile is missed.

--- Assets/Scripts/Piano Tiles/Tile.cs
+++ Assets/Scripts/Piano Tiles/Tile.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Tile : MonoBehaviour
 {
@@ -9,9 +10,29 @@ public class Tile : MonoBehaviour
 	public float tileSpeed = 10f;
 	public SpriteRenderer color;
 
-
 	void Start()
 	{
+
+	}
+
+	void Update()
+    {
+		if (Input.GetKeyDown(KeyCode.A) & Rb.transform.position.y < -2 & Rb.transform.position.y > -6 & Rb.transform.position.x > -3 & Rb.transform.position.x < -1.5)
+		{
+			color.color = Color.yellow;
+		}
+		else if (Input.GetKeyDown(KeyCode.S) & Rb.transform.position.y < -2 & Rb.transform.position.y > -6 & Rb.transform.position.x > -1.5 & Rb.transform.position.x < 0)
+		{
+			color.color = Color.yellow;
+		}
+		else if (Input.GetKeyDown(KeyCode.D) & Rb.transform.position.y < -2 & Rb.transform.position.y > -6 & Rb.transform.position.x > 0 & Rb.transform.position.x < 1.5)
+		{
+			color.color = Color.yellow;
+		}
+		else if (Input.GetKeyDown(KeyCode.F) & Rb.transform.position.y < -2 & Rb.transform.position.y > -6 & Rb.transform.position.x > 1.5 & Rb.transform.position.x < 3)
+		{
+			color.color = Color.yellow;
+		}
 	}
 
 
@@ -26,13 +47,23 @@ public class Tile : MonoBehaviour
 		{
 			Rb.velocity = -transform.up * tileSpeed;
 		}
+
+		if (Rb.transform.position.y < -6 & color.color != Color.yellow)
+        {
+			Time.timeScale = 0;
+			SceneManager.LoadScene(0);
+			Debug.Log("You Lost!");
+		}
 	}
 
 	void OnMouseOver()
     {
-		if (Input.GetMouseButtonDown(0))
-        {
+		Vector2 screenPosition = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+		Vector2 worldPosition = Camera.main.ScreenToWorldPoint(screenPosition);
+
+		if (Input.GetMouseButtonDown(0) & worldPosition.y < -3.5 & worldPosition.y > -4.5 & Rb.transform.position.y < -2 & Rb.transform.position.y > -6)
+		{
 			color.color = Color.yellow;
-        }
+		}
     }
 }
